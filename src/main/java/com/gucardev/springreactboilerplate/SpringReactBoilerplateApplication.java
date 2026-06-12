ef3144845b7076d77b539b6874855cab194c27d9
Gurkan UCAR <ucar.gurkan@hotmail.com>
2026-06-12 23:24:53 +0300

    created

---
+++ src/main/java/com/gucardev/springreactboilerplate/SpringReactBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.springreactboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactBoilerplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactBoilerplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gucardev/springreactboilerplate/SpringReactBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.springreactboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactBoilerplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
